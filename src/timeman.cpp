2a84557100493082f324d68ca3d9cda633ff33f2
Brad Knox <64992190+bknox83@users.noreply.github.com>
2021-06-29 00:20:23 -0500

    time scale tweak

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -74,12 +74,12 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
 
   // x basetime (+ z increment)
   // If there is a healthy increment, timeLeft can exceed actual available
-  // game time for the current move, so also cap to 20% of available game time.
+  // game time for the current move, so also cap to 25% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min(0.0084 + std::pow(ply + 3.0, 0.5) * 0.0042,
-                           0.2 * limits.time[us] / double(timeLeft));
-      maxScale = std::min(7.0, 4.0 + ply / 12.0);
+      optScale = std::min(0.0084 + std::pow(ply + 3.0, 0.5) * 0.0038,
+                           0.25 * limits.time[us] / double(timeLeft));
+      maxScale = 4 + std::pow(ply, 0.25);
   }
 
   // x moves in y seconds (+ z increment)
